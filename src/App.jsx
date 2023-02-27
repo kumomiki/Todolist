4f170eaec4e45dc3a9e260bf7fb053b9b1de6047
kumomiki <rita21625@gmail.com>
2023-02-27 15:54:50 -0600

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Router, Routes } from 'react-router-dom';
 import './App.scss';
-import { TodoPage, LoginPage, SignUpPage } from './pages';
+import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+      <BrowserRouter>
+        <Routes>
+          <Router path="login" element={<LoginPage />} />
+          <Router path="signup" element={<SignUpPage />} />
+          <Router path="todo" element={<TodoPage />} />
+          <Router path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
