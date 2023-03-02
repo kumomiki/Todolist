2f3f47bb0934ef3c1ced38e68d4c98511937b15f
kumomiki <rita21625@gmail.com>
2023-03-02 09:50:24 -0600

    feat: add todo CURD function

---
+++ src/api/todos.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const baseURL = 'http://locaolhost:3001'
+
+const getTodos = () => {}
+const createTodo = () => {}
+
+const patchTodo = () => {}
+const deleteTodo = () => {}
