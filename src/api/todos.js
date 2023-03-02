d3755a768a980eec2bbef7a2661a4b9fc463f801
kumomiki <rita21625@gmail.com>
2023-03-02 09:56:58 -0600

    feat: implement get todos and create todo

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,8 +2,24 @@ import axios from "axios";
 
 const baseURL = 'http://locaolhost:3001'
 
-const getTodos = () => {}
-const createTodo = () => {}
+export const getTodos = async () => {
+  try {
+    const res = await axios.get(`${baseURL}/todos`);
+    return res.data;
+  } catch (error) {
+    console.error(`[Get Todos failed]:`, error);
+  }
+};
+
+export const createTodo = async (payload) => {
+  try {
+    const { title, isEdit} = payload
+    const res = await axios.post(`${baseURL}/todos`, { title, isEdit });
+    return res.data;
+  } catch (error) {
+    console.error(`[Create Todo failed]:`, error);
+  }
+};
 
 const patchTodo = () => {}
 const deleteTodo = () => {}
