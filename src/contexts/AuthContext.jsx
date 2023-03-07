d0a7500a238ff2f7edd252a6a1d76c19b57e732a
kumomiki <rita21625@gmail.com>
2023-03-07 15:20:02 -0600

    feat: add auth function to context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,8 @@
+import { login, register } from '../api/auth';
 import { useState } from 'react';
 import { createContext } from 'react';
+//使用星號（*）來導入一個模組中的所有命名導出
+import * as jwt from 'jsonwebtoken';
 
 const defaultAuthContext = {
   // 使用者是否登入的判斷依據，預設為 false，若取得後端的有效憑證，則切換為 true
@@ -24,6 +27,44 @@ const AuthProvider = ({ children }) => {
       value={{
         isAuthenticated,
         currentMember: payload,
+        register: async (data) => {
+          const { success, authToken } = await register({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        login: async (data) => {
+          const { success, authToken } = await login({
+            username: data.username,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        logout: () => {
+          localStorage.removeItem('authToken');
+          setPayload(null);
+          setIsAuthenticated(false);
+        },
       }}
     >
       {children}
