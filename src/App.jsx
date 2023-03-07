ccc43ce92cae788b2ef212d54ce97a9ff7d8c157
kumomiki <rita21625@gmail.com>
2023-03-07 15:59:30 -0600

    feat: add auth provider to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from 'contexts/AuthContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
@@ -6,12 +7,14 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <AuthProvider>
         <Routes>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
           <Route path="todos" element={<TodoPage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
