8b7cd1618d612233cdbbcf9f0dd4035fee90db56
kumomiki <rita21625@gmail.com>
2023-03-07 10:12:14 -0600

    feat: add register function to signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,11 +8,44 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { register } from 'api/auth';
+import Swal from 'sweetalert2';
 
 const SignUpPage = () => {
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = async () => {
+    // 先判斷輸入的長度是否符合
+    if (username.length === 0) return;
+    if (password.length === 0) return;
+    // 從auth api可獲得success和authToken屬性值，將其取出判斷
+    const { success, authToken } = await register({
+      username,
+      email,
+      password,
+    });
+    // 若成功，會先暫存在localStorage，並顯示登入成功通知
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+      Swal.fire({
+        title: '註冊成功',
+        icon: 'success',
+        showConfirmButton: false,
+        position: 'top',
+        timer: 1000,
+      });
+      return;
+    }
+    Swal.fire({
+      title: '註冊失敗',
+      icon: 'error',
+      showConfirmButton: false,
+      position: 'top',
+      timer: 1000,
+    });
+  };
   return (
     <AuthContainer>
       <div>
@@ -24,8 +57,8 @@ const SignUpPage = () => {
         <AuthInput
           label={'帳號'}
           placeholder={'請輸入帳號'}
-          value={userName}
-          onChange={(nameInputValue) => setUserName(nameInputValue)}
+          value={username}
+          onChange={(nameInputValue) => setUsername(nameInputValue)}
         />
       </AuthInputContainer>
 
@@ -43,12 +76,12 @@ const SignUpPage = () => {
           label={'密碼'}
           placeholder={'請輸入密碼'}
           value={password}
-          type='password'
+          type="password"
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
-      <Link to='/login'>
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
+      <Link to="/login">
         <AuthLinkText>取消</AuthLinkText>
       </Link>
     </AuthContainer>
