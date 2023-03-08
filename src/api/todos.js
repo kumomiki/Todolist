c59d32fec5ed16191b0c00d1c898d1876fdcc390
kumomiki <rita21625@gmail.com>
2023-03-08 16:30:17 -0600

    feat: change todo api

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,32 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001';
+const baseURL = 'https://todo-list.alphacamp.io/api';
+
+//創建一個新的 Axios 實例
+const axiosInstance = axios.create({ baseURL: baseURL });
+
+// axios 提供的 Interceptors 方法，在發出 request 前、收到 response 後產生一些時間差，讓我們可以設定 Auth Token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // 要在Header加上token，而token存在localStorage，因此要取得token
+    const token = localStorage.getItem('authToken');
+    // 若token存在的話，就設立Header
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    // 然後回傳config
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    console.log(error);
+  },
+);
 
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseURL}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseURL}/todos`);
+    return res.data.data;
   } catch (error) {
     console.error(`[Get Todos failed]:`, error);
   }
@@ -14,7 +35,7 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
   try {
-    const res = await axios.post(`${baseURL}/todos`, { title, isDone });
+    const res = await axiosInstance.post(`${baseURL}/todos`, { title, isDone });
     return res.data;
   } catch (error) {
     console.error(`[Create Todo failed]:`, error);
@@ -24,7 +45,10 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseURL}/todos/${id}`, { title, isDone });
+    const res = await axiosInstance.patch(`${baseURL}/todos/${id}`, {
+      title,
+      isDone,
+    });
 
     return res.data;
   } catch (error) {
@@ -33,7 +57,7 @@ export const patchTodo = async (payload) => {
 };
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseURL}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseURL}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error(`[Delete Todo failed]:`, error);
